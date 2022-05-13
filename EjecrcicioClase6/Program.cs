6d44d12fc1f30ef76f7807ee7fb5532b72d104a5
Diana <diana.russiposada@gmail.com>
2022-05-12 22:51:59 -0500

    Adiciones

--- EjecrcicioClase6/Program.cs
+++ EjecrcicioClase6/Program.cs
@@ -1,23 +1,28 @@
 ﻿int numeroSecreto = new
-Random(DateTime.Now.Millisecond).Next(1, 21);
+Random(DateTime.Now.Millisecond).Next(1, 21); // Generar un número secreto
+
+//Declaración e inicialización de variables
 int contador = 1;
 var numero = 0;
 do
 {
-    Console.Write("Ingrese un número entre 1 y 20: ");
-    numero = int.Parse(Console.ReadLine());
+    Console.Write("Ingrese un número entre 1 y 20: "); //Pedir al usuario que ingrese un número
+    numero = int.Parse(Console.ReadLine()); // Guardar número ingresado por el usuario en la variable numero
 
-    
-     if (numero < 0 || numero > 20)
+    //Entra en este if si el número número ingresado no esta entre 1 y 20
+    if (numero < 0 || numero > 20)
     {
         Console.WriteLine("Valor incorrecto . el número que debes adivinar esta entre 1 y 20. Porfavor intenta de nuevo");
-        contador++;
+        contador++; //Aumentar en 1 el número de intentos erroneos
     }
+    //Entra en este if si el número ingresado es mayor al que se debe adivinar
     else if (numero > numeroSecreto)
     {
         Console.WriteLine("Número muy grande. Porfavor intente de nuevo");
-        contador++;
+        contador++; //Aumentar en 1 el número de intentos erroneos
     }
+
+    //Entra en este if si el número ingresado es menor al que se debe adivinar
     else if (numero < numeroSecreto)
     {
         Console.WriteLine("Número muy pequeño. Porfavor intente de nuevo");
@@ -29,3 +34,5 @@ do
     }
 }
 while (numero != numeroSecreto);
+
+// Repo https://github.com/DianaRussi/EjecrcicioClase6.git
