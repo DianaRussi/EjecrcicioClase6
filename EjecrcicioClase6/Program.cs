7a58b53c2f2806500791622efc7b50610102dade
Diana <diana.russiposada@gmail.com>
2022-05-12 23:10:11 -0500

    otras adiciones

--- EjecrcicioClase6/Program.cs
+++ EjecrcicioClase6/Program.cs
@@ -2,37 +2,38 @@
 Random(DateTime.Now.Millisecond).Next(1, 21); // Generar un número secreto
 
 //Declaración e inicialización de variables
-int contador = 1;
-var numero = 0;
+int intentos = 1; //variable para almacenar numero de intentos
+int numero; // Variable que almacena el valor ingresado por el usuario
 do
 {
     Console.Write("Ingrese un número entre 1 y 20: "); //Pedir al usuario que ingrese un número
     numero = int.Parse(Console.ReadLine()); // Guardar número ingresado por el usuario en la variable numero
 
-    //Entra en este if si el número número ingresado no esta entre 1 y 20
     if (numero < 0 || numero > 20)
     {
+        //Imprime este mensaje cuando número número ingresado no esta entre 1 y 20
         Console.WriteLine("Valor incorrecto . el número que debes adivinar esta entre 1 y 20. Porfavor intenta de nuevo");
-        contador++; //Aumentar en 1 el número de intentos erroneos
+        intentos++; //Aumentar en 1 el número de intentos erroneos
     }
-    //Entra en este if si el número ingresado es mayor al que se debe adivinar
     else if (numero > numeroSecreto)
     {
+        //Imprime este mensaje cuando número ingresado es mayor al numero secreto
         Console.WriteLine("Número muy grande. Porfavor intente de nuevo");
-        contador++; //Aumentar en 1 el número de intentos erroneos
+        intentos++; 
     }
 
-    //Entra en este if si el número ingresado es menor al que se debe adivinar
     else if (numero < numeroSecreto)
     {
+        //Imprime este mensaje cuando número ingresado es menor al numero secreto
         Console.WriteLine("Número muy pequeño. Porfavor intente de nuevo");
-        contador++;
+        intentos++;
     }
     else
     {
-        Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}. Lo has logrado en {contador} intentos!!");
+        // Imprime este mensaje cuando se adivine el número secreto
+        Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}. Lo has logrado en {intentos} intentos!!");
     }
 }
-while (numero != numeroSecreto);
+while (numero != numeroSecreto); 
 
 // Repo https://github.com/DianaRussi/EjecrcicioClase6.git
